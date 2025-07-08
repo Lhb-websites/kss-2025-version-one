77f8fd74aa3bd765928670b76a13534a8f5b869b
Lhb-websites <lacidahanzelbusiness@gmail.com>
2025-07-08 14:35:06 +0800

    Fix pricing CTA to scroll to packages section

--- src/pages/Packages.tsx
+++ src/pages/Packages.tsx
@@ -89,7 +89,7 @@ const Packages = () => {
       </section>
 
       {/* Packages Section */}
-      <section className="py-20">
+      <section id="packages" className="py-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
             {packages.map((pkg, index) => (
